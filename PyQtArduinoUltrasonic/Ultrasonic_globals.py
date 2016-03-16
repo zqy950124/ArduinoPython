4279a45c03967a33752e91b332bef6dfe44c2614
Cheng Maohua <cmh@seu.edu.cn>
2016-03-16 17:01:27 +0800

    update rename

--- PyQtArduinoUltrasonic/Ultrasonic_globals.py
+++ PyQtArduinoUltrasonic/Ultrasonic_globals.py
@@ -22,32 +22,32 @@ savedsamples = 100
 average = 0
 
 class LiveDataFeed(object):
-    """ A simple "live data feed" abstraction that allows a reader 
-        to read the most recent data and find out whether it was 
-        updated since the last read. 
-        
+    """ A simple "live data feed" abstraction that allows a reader
+        to read the most recent data and find out whether it was
+        updated since the last read.
+
         Interface to data writer:
-        
+
         add_data(data):
             Add new data to the feed.
-        
+
         Interface to reader:
-        
+
         read_data():
             Returns the most recent data.
-            
+
         has_new_data:
             A boolean attribute telling the reader whether the
-            data was updated since the last read.    
+            data was updated since the last read.
     """
     def __init__(self):
         self.cur_data = None
         self.has_new_data = False
-    
+
     def add_data(self, data):
         self.cur_data = data
         self.has_new_data = True
-    
+
     def read_data(self):
         self.has_new_data = False
         return self.cur_data
@@ -57,7 +57,7 @@ def debug(message1, message2=None, message3=None):
         print (message1, message2, message3)
 
 #===============================================================================
-# partial: very useful function needed when using a connection to a function and 
+# partial: very useful function needed when using a connection to a function and
 # we need to transmit a variable
 #===============================================================================
 if sys.version_info[:2] < (2, 5):
@@ -69,7 +69,7 @@ else:
     from functools import partial
 
 def get_all_from_queue(Q):
-    """ Generator to yield one after the others all items 
+    """ Generator to yield one after the others all items
         currently in the queue Q, without any waiting.
     """
     try:
@@ -81,19 +81,19 @@ def get_all_from_queue(Q):
 def get_item_from_queue(Q, timeout=0.01):
     """ Attempts to retrieve an item from the queue Q. If Q is
         empty, None is returned.
-        
+
         Blocks for 'timeout' seconds in case the queue is empty,
         so don't use this method for speedy retrieval of multiple
         items (use get_all_from_queue for that).
     """
-    try: 
+    try:
         item = Q.get(True, 0.01)
-    except queue.Empty: 
+    except queue.Empty:
         return None
     return item
 
 def enumerate_serial_ports():
-    """ 
+    """
     Purpose:        scan for available serial ports
     Return:         return a list of of the availables ports names
     """
@@ -103,9 +103,9 @@ def enumerate_serial_ports():
             try:
                 s = serial.Serial(i)
                 outAvailablePorts.append(s.portstr)
-                s.close()   
+                s.close()
             except serial.SerialException:
                 pass
         return outAvailablePorts
     else:
-        return glob.glob('/dev/tty.*')
+        return glob.glob('/dev/tty*')
